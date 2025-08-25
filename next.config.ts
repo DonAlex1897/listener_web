d3ba3a0a2f50f7d749bb2a264b9e14fd3abe420f
donalex1897 <kaviyani.ma@gmail.com>
2025-08-25 18:46:36 +0200

    Update Next.js configuration and add export scripts to package.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // required if you use next/image with static export
+  },
 };
 
 export default nextConfig;
